7373edfd496c42e3a8208814579480fec9416ad8
Anusha Ragula <ragulaanushaanu@gmail.com>
2026-09-06 16:26:51 +0530

    Add GitHub Actions test workflow

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,12 +1,4 @@
-from fastapi.testclient import TestClient
-
-from main import app
-
-
-client = TestClient(app)
-
-
-def test_health():
+def test_health(client):
     response = client.get("/health")
 
     assert response.status_code == 200
